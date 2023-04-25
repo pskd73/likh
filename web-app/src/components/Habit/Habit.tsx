c2330064942f65eb749cc7fe020c5ff87fc5145c
Pramod <pramodkumar.damam73@gmail.com>
2023-04-25 13:13:17 +0530

    Add add to calendar

--- web-app/src/components/Habit/Habit.tsx
+++ web-app/src/components/Habit/Habit.tsx
@@ -7,12 +7,59 @@ import Topics from "./Topics";
 import Suggestions from "./Suggestions";
 import ScrollableCol from "../ScrollableCol";
 
+const formatDate = (dateTime: Date) => {
+  return [
+      dateTime.getUTCFullYear(),
+      pad(dateTime.getUTCMonth() + 1),
+      pad(dateTime.getUTCDate()),
+      "T",
+      pad(dateTime.getUTCHours()),
+      pad(dateTime.getUTCMinutes()) + "00Z"
+  ].join("");
+}
+
+const pad = (num: number) => {
+  return num < 10 ? "0" + num : num;
+}
+
 const Habit = () => {
   const { trayOpen, setActiveTray, setTrayOpen } = useContext(AppContext);
 
   const handleTitleClick = () => {
     setActiveTray("habit");
     setTrayOpen(!trayOpen);
+  };
+
+  const handleAddToCalendar = () => {
+    let url = [
+      "BEGIN:VCALENDAR",
+      "VERSION:2.0",
+      "BEGIN:VEVENT",
+      "DTSTART:" + formatDate(new Date()),
+      "DTEND:" + formatDate(new Date()),
+      "SUMMARY:Write on Retro Note",
+      "DESCRIPTION:Build writing habits",
+      "LOCATION:On Retro Note",
+      "RRULE:FREQ=DAILY;COUNT=3;INTERVAL=1",
+      "BEGIN:VALARM",
+      "TRIGGER:-PT15M",
+      "REPEAT:1",
+      "DURATION:PT15M",
+      "ACTION:DISPLAY",
+      "END:VALARM",
+      "END:VEVENT",
+      "END:VCALENDAR",
+    ].join("\n");
+
+    const uri = encodeURI("data:text/calendar;charset=utf8," + url)
+
+    var downloadLink = document.createElement("a");
+    downloadLink.href = uri;
+    downloadLink.download = "Write event.ics";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   };
 
   return (
@@ -34,6 +81,13 @@ const Habit = () => {
             </span>
           </Clickable>
         </Toolbar.Title>
+        <Toolbar.MenuList>
+          <li>
+            <Clickable lite onClick={handleAddToCalendar}>
+              add to calendar &rarr;
+            </Clickable>
+          </li>
+        </Toolbar.MenuList>
       </Toolbar>
     </div>
   );
