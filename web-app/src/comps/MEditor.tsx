bc7f0721d7b65c058f7a926029dbd323a3820e91
Pramod <pramodkumar.damam73@gmail.com>
2023-05-10 23:21:19 +0530

    Add title

--- web-app/src/comps/MEditor.tsx
+++ web-app/src/comps/MEditor.tsx
@@ -2,7 +2,14 @@ import classNames from "classnames";
 import Prism from "prismjs";
 import "prismjs/components/prism-markdown";
 import { useCallback, useState } from "react";
-import { createEditor, BaseEditor, NodeEntry, BaseRange, Text } from "slate";
+import {
+  createEditor,
+  BaseEditor,
+  NodeEntry,
+  BaseRange,
+  Range,
+  Text,
+} from "slate";
 import { Slate, Editable, withReact, ReactEditor } from "slate-react";
 
 type CustomElement = { type: "paragraph"; children: CustomText[] };
@@ -16,32 +23,45 @@ declare module "slate" {
 }
 
 const initialValue = [
+  // {
+  //   type: "paragraph",
+  //   children: [
+  //     {
+  //       text: "Slate is flexible enough to add **decorations** that can format text based on its content. For example, this editor has **Markdown** preview decorations on it, to make it _dead_ simple to make an editor with built-in Markdown previewing.",
+  //     },
+  //   ],
+  // },
+  // {
+  //   type: "paragraph",
+  //   children: [{ text: "## Try it out!" }],
+  // },
+  // {
+  //   type: "paragraph",
+  //   children: [{ text: "Try it out for yourself!" }],
+  // },
   {
     type: "paragraph",
-    children: [
-      {
-        text: "Slate is flexible enough to add **decorations** that can format text based on its content. For example, this editor has **Markdown** preview decorations on it, to make it _dead_ simple to make an editor with built-in Markdown previewing.",
-      },
-    ],
+    children: [{ text: "**Hello!** How are you? *go*!" }],
   },
   {
     type: "paragraph",
-    children: [{ text: "## Try it out!" }],
-  },
-  {
-    type: "paragraph",
-    children: [{ text: "Try it out for yourself!" }],
+    children: [{ text: "## This is title" }],
   },
 ];
 
 const Leaf = ({ attributes, children, leaf }: any) => {
+  console.log(leaf);
   return (
     <span
       {...attributes}
       className={classNames({
-        "font-bold": leaf.bold,
+        "font-semibold": leaf.bold,
         italic: leaf.italic,
-        "text-2xl inline-block mt-4 mb-2": leaf.title,
+        "inline-block mt-6 mb-2": leaf.title,
+        "text-xl": leaf.title && leaf.titleLevel === 1,
+        "text-2xl": leaf.title && leaf.titleLevel === 2,
+        "text-3xl": leaf.title && leaf.titleLevel === 3,
+        "opacity-30": leaf.punctuation,
       })}
     >
       {children}
@@ -53,7 +73,7 @@ const MEditor = () => {
   const [editor] = useState(() => withReact(createEditor()));
   const renderLeaf = useCallback((props: any) => <Leaf {...props} />, []);
   const decorate = useCallback(([node, path]: NodeEntry) => {
-    const ranges: BaseRange[] = [];
+    const ranges: Range[] = [];
 
     if (!Text.isText(node)) {
       return ranges;
@@ -80,11 +100,41 @@ const MEditor = () => {
       const end = start + length;
 
       if (typeof token !== "string") {
-        ranges.push({
-          [token.type]: true,
-          anchor: { path, offset: start },
-          focus: { path, offset: end },
-        });
+        if (Array.isArray(token.content)) {
+          let _start = start;
+          let titleLevel = token.type === "title" ? undefined : null;
+
+          for (const item of token.content) {
+            const _length = getLength(item);
+            const _end = _start + _length;
+
+            if (
+              token.type === "title" &&
+              (item as any).type === "punctuation" &&
+              titleLevel === undefined
+            ) {
+              titleLevel = _length;
+            }
+
+            // console.log(item, _length, _start, _end);
+
+            ranges.push({
+              [token.type]: true,
+              [(item as any).type]: true,
+              titleLevel,
+              anchor: { path, offset: _start },
+              focus: { path, offset: _end },
+            } as any);
+
+            _start = _end;
+          }
+        } else {
+          ranges.push({
+            [token.type]: true,
+            anchor: { path, offset: start },
+            focus: { path, offset: end },
+          });
+        }
       }
 
       start = end;
@@ -94,9 +144,11 @@ const MEditor = () => {
   }, []);
 
   return (
-    <Slate editor={editor} value={initialValue as any}>
-      <Editable decorate={decorate} renderLeaf={renderLeaf} />
-    </Slate>
+    <div className="text-[20px] font-CourierPrime leading-8">
+      <Slate editor={editor} value={initialValue as any}>
+        <Editable decorate={decorate} renderLeaf={renderLeaf} />
+      </Slate>
+    </div>
   );
 };
 
