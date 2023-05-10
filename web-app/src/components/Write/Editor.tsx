ce76237333e378cd9772e78bd6b6eaaf05fdbe6a
Pramod Kumar <pramodkumar.damam73@gmail.com>
2023-05-10 10:38:47 +0530

    Set background color for textarea (#6)

--- web-app/src/components/Write/Editor.tsx
+++ web-app/src/components/Write/Editor.tsx
@@ -79,7 +79,7 @@ const Editor = ({
       <textarea
         value={note.title}
         ref={titleRef}
-        className="text-3xl outline-none w-full resize-none dark:bg-iblack font-bold"
+        className="text-3xl outline-none w-full resize-none font-bold bg-base"
         onChange={handleTitleChange}
         style={{ marginBottom: TITLE_MARGIN_BOTTOM }}
       />
@@ -87,7 +87,7 @@ const Editor = ({
         ref={textRef}
         value={note.text}
         onChange={handleTextChange}
-        className="outline-none w-full scrollbar-hide overflow-y-scroll resize-none dark:bg-iblack"
+        className="outline-none w-full scrollbar-hide overflow-y-scroll resize-none bg-base"
       />
     </div>
   );
