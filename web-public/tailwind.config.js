7bc1b3787f02882bd27161fcc0c69c1bf3268670
Pramod <pramodkumar.damam73@gmail.com>
2023-05-14 01:49:30 +0530

    Remove bloat code

--- web-public/tailwind.config.js
+++ web-public/tailwind.config.js
@@ -7,11 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         "primary-700": "#6C6327",
         "primary-500": "#CDC596",
