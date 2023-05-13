409203506dac0f283f62a406a54f23c83fe6fc38
Pramod Kumar <pramodkumar.damam73@gmail.com>
2023-05-14 01:50:29 +0530

    Setup NextJS app for public (#16)
    * Setup NextJS app for public
    
    * Remove app/page.tsx
    
    * Remove bloat code

---
+++ web-public/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-700": "#6C6327",
+        "primary-500": "#CDC596",
+        "primary-400": "#E9E3C2",
+        "base": "#FFFEF8"
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "*": {
+              color: theme("colors.primary-700")
+            },
+          }
+        }
+      })
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
+}
