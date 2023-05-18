768a8e850cf9697e730d2f0e3eea843d7a50a446
Pramod <pramodkumar.damam73@gmail.com>
2023-05-18 13:09:44 +0530

    Update public host on app

--- web-app/src/config.ts
+++ web-app/src/config.ts
@@ -2,4 +2,4 @@ export const API_HOST = "https://api.retronote.app";
 export const SUPABASE_URL = "https://gfbrmxfdddmpwlqtvwsh.supabase.co";
 export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmYnJteGZkZGRtcHdscXR2d3NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3NzMwNjQsImV4cCI6MTk5ODM0OTA2NH0.44lRQgUo7MOULLBvJ3moD6Z_XhA7FOcxqCA0kH22H9M";
 export const HOST = "https://retronote.app";
-export const PUBLIC_HOST = "https://read.retronote.app"
+export const PUBLIC_HOST = "https://retronote.app"
