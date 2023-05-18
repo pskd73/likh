9c6d54244e1161ed32b8ce5b07169d2e63cf734b
Pramod <pramodkumar.damam73@gmail.com>
2023-05-18 14:18:22 +0530

    Increase max lenght of the title to 100

--- web-app/src/Note.ts
+++ web-app/src/Note.ts
@@ -5,14 +5,16 @@ export const getNoteTitle = (note: Note) => {
     return note.title;
   }
   if (note.text) {
+    const MAX_LENGTH = 100;
+
     let cleaned = note.text;
     const titleMatch = cleaned.match(/^\n*#{1,3} (.*)\n*.*/);
     if (titleMatch) {
       cleaned = titleMatch[1];
     }
     return (
-      cleaned.replaceAll("\n", " ").substring(0, 50) +
-      (cleaned.length > 50 ? "..." : "")
+      cleaned.replaceAll("\n", " ").substring(0, MAX_LENGTH) +
+      (cleaned.length > MAX_LENGTH ? "..." : "")
     );
   }
   return note.title;
