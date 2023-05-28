df347b0c6ad481612948b3d838d76b1d9a441411
Pramod <pramodkumar.damam73@gmail.com>
2023-05-28 15:18:11 +0530

    Add prev and next shortcuts

--- web-app/src/comps/Editor/useShortcuts.tsx
+++ web-app/src/comps/Editor/useShortcuts.tsx
@@ -8,12 +8,33 @@ const isWindowShortcut = (e: KeyboardEvent) => {
 const shortcuts: Record<string, (editor: EditorContextType) => void> = {
   l: (editor) => editor.toggleSideBar(),
   n: (editor) => editor.newNote({ text: "New note" }),
+  ArrowLeft: (editor) => {
+    const { storage } = editor;
+    const idx = storage.notes.findIndex((note) => editor.note.id === note.id);
+    const prevNote = storage.notes[idx - 1]
+      ? storage.getNote(storage.notes[idx - 1].id)
+      : undefined;
+    if (prevNote) {
+      editor.updateNote(prevNote);
+    }
+  },
+  ArrowRight: (editor) => {
+    const { storage } = editor;
+    const idx = storage.notes.findIndex((note) => editor.note.id === note.id);
+    const nextNote = storage.notes[idx + 1]
+      ? storage.getNote(storage.notes[idx + 1].id)
+      : undefined;
+    if (nextNote) {
+      editor.updateNote(nextNote);
+    }
+  },
 };
 
 const useShortcuts = (editor: EditorContextType) => {
   const handleKeyDown = useCallback(
     (e: KeyboardEvent) => {
       if (isWindowShortcut(e)) {
+        console.log(e);
         if (shortcuts[e.key]) {
           e.preventDefault();
           shortcuts[e.key](editor);
