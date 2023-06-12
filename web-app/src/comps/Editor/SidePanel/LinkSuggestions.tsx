0857b9604c16c578ae97ae1f5163fe08f9158aaf
Pramod <pramodkumar.damam73@gmail.com>
2023-06-12 23:15:52 +0530

    Add search button on link suggestion

--- web-app/src/comps/Editor/SidePanel/LinkSuggestions.tsx
+++ web-app/src/comps/Editor/SidePanel/LinkSuggestions.tsx
@@ -1,17 +1,23 @@
 import { useContext, useMemo, useState } from "react";
 import { EditorContext } from "../Context";
 import classNames from "classnames";
-import Collapsible from "../Collapsible";
 import { LinkSuggestion } from "../Suggestion";
 import { isMobile } from "../device";
 import { titleCase } from "../../../Note";
+import { BiSearch } from "react-icons/bi";
+import Button from "../../Button";
 
 const BASE_FONT_SIZE = 12;
 const MULTIPLIER = 3;
 
 const LinkSuggestions = () => {
-  const { getLinkSuggestions, setSideBar, setOrNewNote, note } =
-    useContext(EditorContext);
+  const {
+    getLinkSuggestions,
+    setSideBar,
+    setOrNewNote,
+    note,
+    setSearchTerm,
+  } = useContext(EditorContext);
   const [update, setUpdate] = useState(new Date().getTime());
   const suggestions = useMemo(() => getLinkSuggestions(), [update, note.id]);
 
@@ -23,6 +29,11 @@ const LinkSuggestions = () => {
     setUpdate(new Date().getTime());
   };
 
+  const handleSearch = (suggestion: LinkSuggestion) => {
+    setSearchTerm(suggestion.text);
+    setSideBar("explorer");
+  };
+
   return (
     <div className="p-2 py-4">
       <ul className="flex gap-2 flex-wrap" style={{ fontSize: BASE_FONT_SIZE }}>
@@ -30,7 +41,7 @@ const LinkSuggestions = () => {
           <li
             className={classNames(
               "bg-primary-700 bg-opacity-10 py-1 px-2 rounded-lg inline-block space-x-2",
-              "flex items-center"
+              "flex items-center group"
             )}
             style={{
               fontSize:
@@ -44,6 +55,15 @@ const LinkSuggestions = () => {
               {suggestion.text}
             </span>
             <span className="opacity-50">({suggestion.occurances})</span>
+            <span className="flex items-center">
+              <Button
+                lite
+                className="rounded-lg"
+                onClick={() => handleSearch(suggestion)}
+              >
+                <BiSearch />
+              </Button>
+            </span>
           </li>
         ))}
       </ul>
