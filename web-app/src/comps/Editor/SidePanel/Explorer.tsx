e4cb4daea59340a1fdb4c3fce4239328b47ca36c
Pramod <pramodkumar.damam73@gmail.com>
2023-06-19 15:41:54 +0530

    Add underline for twitter urls

--- web-app/src/comps/Editor/SidePanel/Explorer.tsx
+++ web-app/src/comps/Editor/SidePanel/Explorer.tsx
@@ -376,7 +376,7 @@ const Explorer = () => {
       <div className="mt-2 text-xs">
         <span className="opacity-40">Built by </span>
         <a
-          className="opacity-40 hover:opacity-100 hover:underline"
+          className="opacity-40 hover:opacity-100 underline"
           href="https://twitter.com/@pramodk73"
           target="_blank"
           rel="noreferrer"
@@ -389,7 +389,7 @@ const Explorer = () => {
       <div className="mb-2 text-xs">
         <span className="opacity-40">Join the </span>
         <a
-          className="opacity-40 hover:opacity-100 hover:underline"
+          className="opacity-40 hover:opacity-100 underline"
           href="https://twitter.com/i/communities/1670013921598795778"
           target="_blank"
           rel="noreferrer"
