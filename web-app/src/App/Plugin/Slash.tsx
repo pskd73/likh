dc951bc11513f09abc3ba381be8b1055c62dcf51
Pramod <pramodkumar.damam73@gmail.com>
2023-07-16 02:49:40 +0530

    Slash for inline as well

--- web-app/src/App/Plugin/Slash.tsx
+++ web-app/src/App/Plugin/Slash.tsx
@@ -8,42 +8,53 @@ const SlashPlugin: RNPluginCreator = () => {
     suggestions: {
       "/": {
         suggest: (prefix, word, note, range) => {
-          if (range.anchor.offset !== 0) return [];
           const suggestions: Suggestion[] = [];
+          if (range.anchor.offset === 0) {
+            suggestions.push({
+              title: "Heading 1",
+              replace: "# ",
+              description: "Insert markdown for Heading 1",
+            });
+            suggestions.push({
+              title: "Heading 2",
+              replace: "## ",
+              description: "Insert markdown for Heading 2",
+            });
+            suggestions.push({
+              title: "Heading 3",
+              replace: "### ",
+              description: "Insert markdown for Heading 3",
+            });
+            suggestions.push({
+              title: "Bullet list",
+              replace: "* ",
+              description: "Start unordered bullt list",
+            });
+            suggestions.push({
+              title: "Number list",
+              replace: "1. ",
+              description: "Start ordered number list",
+            });
+            suggestions.push({
+              title: "Check box",
+              replace: "- [ ] ",
+              description: "Start checkbox list",
+            });
+            suggestions.push({
+              title: "Quote",
+              replace: "> ",
+              description: "Block quote",
+            });
+          }
           suggestions.push({
-            title: "Heading 1",
-            replace: "# ",
-            description: "Insert markdown for Heading 1",
+            title: "Timestamp",
+            replace: "@",
+            description: "Insert timestamp",
           });
           suggestions.push({
-            title: "Heading 2",
-            replace: "## ",
-            description: "Insert markdown for Heading 2",
-          });
-          suggestions.push({
-            title: "Heading 3",
-            replace: "### ",
-            description: "Insert markdown for Heading 3",
-          });
-          suggestions.push({
-            title: "Bullet list item",
-            replace: "* ",
-            description: "Unordered list item",
-          });
-          suggestions.push({
-            title: "Numeric list item",
-            replace: "1. ",
-            description: "Numeric list item",
-          });
-          suggestions.push({
-            title: "Checklist item",
-            replace: "- [ ] ",
-            description: "Insert a checkbox list item",
-          });
-          suggestions.push({
-            title: "Quote",
-            replace: "> ",
-            description: "Block quote",
+            title: "Note",
+            replace: "[[",
+            description: "Link another note",
           });
           return suggestions.filter((sug) =>
             sug.title.toLowerCase().includes(word)
