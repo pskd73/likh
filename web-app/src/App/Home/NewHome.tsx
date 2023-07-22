06e1b4eea0e58cf91c18ea7e0d49aaa1f4708fc9
Pramod <pramodkumar.damam73@gmail.com>
2023-07-22 15:48:08 +0530

    Add tiles

--- web-app/src/App/Home/NewHome.tsx
+++ web-app/src/App/Home/NewHome.tsx
@@ -1,5 +1,22 @@
 import moment from "moment";
 import { useEffect, useRef, useState } from "react";
+import { getShortcutText } from "../useShortcuts";
+import {
+  BiCalendarAlt,
+  BiCheckCircle,
+  BiGhost,
+  BiRightArrowAlt,
+} from "react-icons/bi";
+import {
+  TfiCheck,
+  TfiPencilAlt,
+  TfiImport,
+  TfiCalendar,
+  TfiArrowRight,
+} from "react-icons/tfi";
+import classNames from "classnames";
+import { ReactElement } from "react-markdown/lib/react-markdown";
+import { Link } from "react-router-dom";
 
 const DateTime = () => {
   const [time, setTime] = useState(new Date());
@@ -37,11 +54,98 @@ const DateTime = () => {
   );
 };
 
+const ClickableTile = ({
+  label,
+  description,
+  icon,
+  onClick,
+  rightIcon,
+}: {
+  label: string;
+  description: string;
+  icon?: ReactElement;
+  onClick: () => void;
+  rightIcon?: ReactElement;
+}) => {
+  return (
+    <Link
+      to="/"
+      className={classNames(
+        "flex justify-between items-center",
+        "border border-primary border-opacity-10 rounded-lg",
+        "py-4 bg-primary bg-opacity-5 active:bg-opacity-10 hover:shadow-md",
+        "cursor-pointer active:shadow transition-all",
+        "px-6"
+      )}
+    >
+      <div className="flex items-center">
+        {icon && <div className="text-4xl mr-6">{icon}</div>}
+        <div className="flex flex-col pr-4">
+          <div className="text-xl font-bold">{label}</div>
+          <div className="text-sm text-primary text-opacity-50">
+            {description}
+          </div>
+        </div>
+      </div>
+      {rightIcon && <div className="text-2xl">{rightIcon}</div>}
+    </Link>
+  );
+};
+
 const NewHome = () => {
   return (
     <div>
-      <div className="flex justify-end">
+      <div className="flex justify-end mb-10">
         <DateTime />
+      </div>
+      <div className="lg:flex space-x-6">
+        <div className="flex-1">
+          <div className="mb-4">
+            <ClickableTile
+              label="My personal note"
+              description="Edited 2 hours ago"
+              onClick={console.log}
+              rightIcon={<TfiArrowRight />}
+            />
+          </div>
+          <div className="lg:grid lg:grid-cols-2 gap-4 space-y-4 lg:space-y-0">
+            <div>
+              <ClickableTile
+                label="Write new"
+                description={getShortcutText("N")}
+                icon={<TfiPencilAlt />}
+                onClick={console.log}
+              />
+            </div>
+            <div>
+              <ClickableTile
+                label="Journal"
+                description={getShortcutText("J")}
+                icon={<TfiCalendar />}
+                onClick={console.log}
+              />
+            </div>
+            <div>
+              <ClickableTile
+                label="Todo"
+                description={getShortcutText("T")}
+                icon={<TfiCheck />}
+                onClick={console.log}
+              />
+            </div>
+            <div>
+              <ClickableTile
+                label="Import"
+                description={getShortcutText("O")}
+                icon={<TfiImport />}
+                onClick={console.log}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-1/3 p-4 border border-primary border-opacity-20 rounded-md">
+          Scribble
+        </div>
       </div>
     </div>
   );
