be4f1b74d449d6110d81499462331d4f9d207135
Pramod <pramodkumar.damam73@gmail.com>
2023-08-23 10:25:46 +0530

    Add discord link

--- web-app/src/App/Home/Promotion.tsx
+++ web-app/src/App/Home/Promotion.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps } from "react";
-import { RxTwitterLogo, RxEnvelopeClosed } from "react-icons/rx";
+import { RxTwitterLogo, RxEnvelopeClosed, RxDiscordLogo } from "react-icons/rx";
 
 const SocialIcon = ({ children, ...restProps }: ComponentProps<"a">) => {
   return (
@@ -30,11 +30,14 @@ const Promotion = () => {
 
       {/* Community */}
       <div className="mb-2 px-2 text-3xl flex items-center space-x-6">
-        <SocialIcon href="https://twitter.com/retronote_app">
+        <SocialIcon href="https://twitter.com/retronote_app" target="_blank">
           <RxTwitterLogo />
         </SocialIcon>
         <SocialIcon href="mailto:help@retronote.app">
           <RxEnvelopeClosed />
+        </SocialIcon>
+        <SocialIcon href="https://discord.gg/wqThG6K5f" target="_blank">
+          <RxDiscordLogo />
         </SocialIcon>
         <div className="flex justify-center items-center">
           <a
